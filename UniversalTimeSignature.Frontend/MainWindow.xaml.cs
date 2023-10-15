8028e508b856303ac864ba25138a8b11078a8beb
silicondev <josh@phyre.uk>
2023-10-15 23:59:14 +0100

    Current attempts at getting the algorithm to run within a reasonable timescale. Needs optimising

--- UniversalTimeSignature.Frontend/MainWindow.xaml.cs
+++ UniversalTimeSignature.Frontend/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UniversalTimeSignature.Lib;
 
 namespace UniversalTimeSignature
 {
@@ -23,6 +24,10 @@ namespace UniversalTimeSignature
         public MainWindow()
         {
             InitializeComponent();
+            MessageBox.Show(DateTime.Now.ToUTS());
+            //string str = "a";
+            //str = str.Swap(0, 'b');
+            //MessageBox.Show(str);
         }
     }
 }
---
+++ UniversalTimeSignature.Lib/UTS.cs
@@ -0,0 +1,129 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace UniversalTimeSignature.Lib
+{
+    public static class UTS
+    {
+        private static string ch = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public static string ToUTS(this DateTime dt)
+        {
+            long t = dt.Ticks;
+            string str = "";
+            //int index = 0;
+            var units = new List<int>() { 0 };
+
+            int pow = 1;
+
+            while (t > Math.Pow(ch.Length, pow))
+                pow++;
+
+            pow--;
+
+            for (int p = 0; p < pow - 1; p++)
+            {
+                // THIS WONT WORK YET
+                int i = p;
+                for (; ; )
+                {
+                    if (units.Count == i)
+                    {
+                        units.Add(1);
+                        break;
+                    }
+                    else
+                    {
+                        units[i]++;
+                        if (units[i] < 62)
+                            break;
+                        else
+                            units[i] = 0;
+                    }
+                    i++;
+                }
+            }
+
+            while (t > 0)
+            {
+                //if (str.Length < index + 1)
+                //{
+                //    str += ch[1];
+                //    index = 0;
+                //}
+                //else
+                //{
+                //    int chi = ch.IndexOf(str[index]) + 1;
+                //    if (chi >= ch.Length)
+                //    {
+                //        str = str.Swap(index, ch[0]);
+                //        index++;
+                //        t++;
+                //    }
+                //    else
+                //    {
+                //        str = str.Swap(index, ch[chi]);
+                //        index = 0;
+                //    }
+                //}
+
+                int i = 0;
+                for ( ; ; )
+                {
+                    if (units.Count == i)
+                    {
+                        units.Add(1);
+                        break;
+                    }
+                    else
+                    {
+                        units[i]++;
+                        if (units[i] < 62)
+                            break;
+                        else
+                            units[i] = 0;
+                    }
+                    i++;
+                }
+
+                t--;
+            }
+
+            foreach (int unit in units)
+                str += ch[unit];
+
+            return str.Reverse().ToString();
+        }
+
+        private static List<int> Advance(List<int> l, int max, int index = 0)
+        {
+            var list = new List<int>(l);
+
+            if (list.Count > index)
+            {
+                list[index]++;
+                if (list[index] >= max)
+                {
+                    list[index] = 0;
+                    return Advance(list, max, index + 1);
+                }
+            }
+            else
+                list.Add(1);
+
+            return list;
+        }
+
+        //public static string Swap(this string str, int index, char ch) =>
+        //    str.Remove(index, 1).Insert(index, ch.ToString());
+
+        public static string Swap(this string str, int index, char ch)
+        {
+            string s = str;
+            s = s.Remove(index, 1);
+            s = s.Insert(index, ch.ToString());
+            return s;
+        }
+    }
+}
